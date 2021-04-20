d79260bdc1456aaaf907be63f2a0da078092129a
Miles Egan <miles@plastaq.com>
2021-04-20 19:16:41 +0700

    Basic tag reading works.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+extern crate diacritics;
+extern crate metaflac;
+
+use metaflac::Tag;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if let Some(file) = args.get(1) {
+        let mut tag = Tag::read_from_path(&file).unwrap();
+
+        for comment in &tag.vorbis_comments_mut().comments {
+            println!(
+                "{}: {}",
+                comment.0,
+                diacritics::remove_diacritics(&comment.1[0])
+            );
+        }
+
+        tag.save().unwrap();
+    } else {
+        println!("No file specified.")
+    }
 }
