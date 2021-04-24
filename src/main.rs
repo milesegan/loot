9858fa888723b96f4a3a30389e9013918bcc9b7e
Miles Egan <miles@plastaq.com>
2021-04-24 08:02:02 +0700

    Refactor.

--- src/main.rs
+++ src/main.rs
@@ -1,73 +1,100 @@
 extern crate diacritics;
+extern crate globwalk;
 extern crate metaflac;
 extern crate opus_headers;
+extern crate unicode_normalization;
 
-use glob::glob;
-use metaflac::Tag;
+// use metaflac::Tag;
 // use opus_headers;
 use std::env;
 use std::path::Path; // or parse_from_read or parse_from_file
+use unicode_normalization::UnicodeNormalization;
 
-fn tidy_string(string: &str) -> String {
-    return diacritics::remove_diacritics(string);
+enum FileType {
+    Opus,
+    Flac,
 }
 
-fn process_file_flac(base: &str, path: &str) -> Option<i32> {
-    let mut tag = Tag::read_from_path(&path).ok()?;
-    let comments = &tag.vorbis_comments_mut().comments;
-    let artist = &comments.get("ALBUMARTIST")?.get(0)?; //["ALBUMARTIST"];
-    let album = &comments.get("ALBUM")?.get(0)?;
-    let track = &comments.get("TITLE")?.get(0)?;
-    let number = &comments.get("TRACKNUMBER")?.get(0)?;
+struct Tag {
+    artist: String,
+    album: String,
+    track: String,
+    number: i8,
+}
+
+impl Tag {
+    fn read(path: &str) -> Option<Tag> {
+        match file_type(path)? {
+            FileType::Flac => Tag::read_flac(path),
+            FileType::Opus => Tag::read_opus(path),
+        }
+    }
+
+    fn read_flac(path: &str) -> Option<Tag> {
+        let mut tag = metaflac::Tag::read_from_path(&path).ok()?;
+        let comments = &tag.vorbis_comments_mut().comments;
+        let artist = &comments.get("ALBUMARTIST")?.get(0)?;
+        let album = &comments.get("ALBUM")?.get(0)?;
+        let track = &comments.get("TITLE")?.get(0)?;
+        let number = &comments.get("TRACKNUMBER")?.get(0)?.parse::<i8>().ok()?;
+
+        return Some(Tag {
+            artist: artist.to_owned().to_owned(),
+            album: album.to_owned().to_owned(),
+            track: track.to_owned().to_owned(),
+            number: number.to_owned(),
+        });
+    }
+
+    fn read_opus(path: &str) -> Option<Tag> {
+        let headers = opus_headers::parse_from_path(path).unwrap();
+        let comments = headers.comments.user_comments;
+        let artist = &comments.get("ALBUMARTIST")?;
+        let album = &comments.get("ALBUM")?;
+        let track = &comments.get("TITLE")?;
+        let number = &comments.get("TRACKNUMBER")?.parse::<i8>().ok()?;
+
+        return Some(Tag {
+            artist: artist.to_owned().to_owned(),
+            album: album.to_owned().to_owned(),
+            track: track.to_owned().to_owned(),
+            number: number.to_owned(),
+        });
+    }
+}
+
+fn file_type(path: &str) -> Option<FileType> {
+    let ext = Path::new(&path).extension()?.to_str()?;
+
+    return match ext {
+        "flac" => Some(FileType::Flac),
+        "opus" => Some(FileType::Opus),
+        _ => None,
+    };
+}
+
+fn tidy_string(string: &str) -> String {
+    return diacritics::remove_diacritics(string).nfc().collect();
+}
+
+fn process_file(base: &str, path: &str) -> Option<i32> {
+    let tag = Tag::read(path)?;
 
     let nicedir = format!(
         "{}/{}/{}",
         tidy_string(base),
-        tidy_string(artist),
-        tidy_string(album)
+        tidy_string(&tag.artist),
+        tidy_string(&tag.album)
     );
 
     let nicepath = format!(
         "{}/{:0>2} {}.flac",
         nicedir,
-        number,
-        tidy_string(track).replace("/", "-")
+        tag.number,
+        tidy_string(&tag.track).replace("/", "-")
     );
 
-    println!("{}", path);
-    if path != nicepath {
-        println!("{} -> {}", path, nicepath);
-        // std::fs::create_dir_all(&nicedir).expect("can't make directories");
-        // std::fs::rename(&path, &nicepath).expect("rename failed");
-    }
-
-    return Some(0);
-}
-
-fn process_file_opus(base: &str, path: &str) -> Option<i32> {
-    let headers = opus_headers::parse_from_path(path).unwrap();
-    let comments = headers.comments.user_comments;
-
-    let artist = &comments.get("ALBUMARTIST")?;
-    let album = &comments.get("ALBUM")?;
-    let track = &comments.get("TITLE")?;
-    let number = &comments.get("TRACKNUMBER")?;
-
-    let nicedir = format!(
-        "{}/{}/{}",
-        tidy_string(base),
-        tidy_string(artist),
-        tidy_string(album)
-    );
-
-    let nicepath = format!(
-        "{}/{:0>2} {}.opus",
-        nicedir,
-        number,
-        tidy_string(track).replace("/", "-")
-    );
-
-    if path != nicepath {
+    if path.nfc().collect::<String>() != nicepath {
         println!("{} -> {}", path, nicepath);
         std::fs::create_dir_all(&nicedir).expect("can't make directories");
         std::fs::rename(&path, &nicepath).expect("rename failed");
@@ -82,11 +109,12 @@ fn main() {
 
     let canonical = Path::new(path).canonicalize().expect("Invalid path.");
     let canonical_string = canonical.to_str().expect("Invalid path.");
-    println!("{}", canonical_string);
+    println!("processing {}", canonical_string);
 
-    let pattern = format!("{}/**/*.opus", canonical_string);
-    let entries = glob(&pattern).expect("Illegal glob pattern.");
-    for entry in entries {
-        process_file_opus(&canonical_string, &entry.unwrap().to_str().unwrap());
+    let pattern = format!("{}/**/*.{{flac,opus}}", canonical_string);
+    for entry in globwalk::glob(pattern).expect("Glob error.") {
+        if let Ok(entry) = entry {
+            process_file(&canonical_string, &entry.path().to_str().unwrap());
+        }
     }
 }
