abb49c071c6d8b0bb0c68c01c6d5cb7fd292d4d3
Miles Egan <miles@plastaq.com>
2021-04-26 08:48:50 +0700

    Refactor into library.

--- src/main.rs
+++ src/main.rs
@@ -1,150 +1,24 @@
-extern crate diacritics;
-extern crate globwalk;
-extern crate metaflac;
-extern crate opus_headers;
-extern crate unicode_normalization;
-
 use regex::Regex;
-use std::collections::HashMap;
 use std::env;
-use std::io;
-use std::num;
 use std::path::Path;
 use unicode_normalization::UnicodeNormalization;
 
-enum TagError {
-    ReadError,
-}
-
-impl From<num::ParseIntError> for TagError {
-    fn from(_err: num::ParseIntError) -> TagError {
-        TagError::ReadError
-    }
-}
-
-impl From<io::Error> for TagError {
-    fn from(_err: io::Error) -> TagError {
-        TagError::ReadError
-    }
-}
-
-impl From<opus_headers::ParseError> for TagError {
-    fn from(_err: opus_headers::ParseError) -> TagError {
-        TagError::ReadError
-    }
-}
-
-impl From<metaflac::Error> for TagError {
-    fn from(_err: metaflac::Error) -> TagError {
-        TagError::ReadError
-    }
-}
-
-type Result<T> = std::result::Result<T, TagError>;
-
-enum FileType {
-    Opus,
-    Flac,
-}
-
-struct Tag {
-    album: String,
-    artist: String,
-    // disc: Option<i8>,
-    number: i32,
-    track: String,
-}
-
-fn parse_number(tag: &str) -> Result<i32> {
-    let number_regex = Regex::new(r"[^0-9].*").unwrap();
-    match number_regex.replace_all(tag, "").parse::<i32>() {
-        Ok(number) => Ok(number),
-        Err(_) => Err(TagError::ReadError),
-    }
-}
-
-impl Tag {
-    fn read(path: &str) -> Result<Tag> {
-        match file_type(path) {
-            Some(FileType::Flac) => Tag::read_flac(path),
-            Some(FileType::Opus) => Tag::read_opus(path),
-            _ => Err(TagError::ReadError),
-        }
-    }
-
-    fn read_flac(path: &str) -> Result<Tag> {
-        fn extract_tag<'a>(tag: &'a HashMap<String, Vec<String>>, key: &str) -> Result<&'a String> {
-            return tag
-                .get(key)
-                .ok_or(TagError::ReadError)?
-                .get(0)
-                .ok_or(TagError::ReadError);
-        }
-        let mut tag = metaflac::Tag::read_from_path(&path)?;
-        let comments = &tag.vorbis_comments_mut().comments;
-        let artist = extract_tag(&comments, "ARTIST")?;
-        let album = extract_tag(&comments, "album")?;
-        let track = extract_tag(&comments, "TITLE")?;
-        let number = extract_tag(&comments, "TRACKNUMBER").and_then(|t| parse_number(t))?;
-
-        return Ok(Tag {
-            artist: artist.to_owned(),
-            album: album.to_owned(),
-            // disc: None,
-            number: number.to_owned(),
-            track: track.to_owned(),
-        });
-    }
-
-    fn read_opus(path: &str) -> Result<Tag> {
-        let headers = opus_headers::parse_from_path(path)?;
-        let comments = headers.comments.user_comments;
-        let artist = comments.get("ARTIST").ok_or(TagError::ReadError)?;
-        let album = comments.get("ALBUM").ok_or(TagError::ReadError)?;
-        let track = comments.get("TITLE").ok_or(TagError::ReadError)?;
-        let number = comments
-            .get("TRACKNUMBER")
-            .ok_or(TagError::ReadError)
-            .and_then(|t| parse_number(t))?;
-
-        return Ok(Tag {
-            artist: artist.to_owned(),
-            album: album.to_owned(),
-            // disc: None,
-            number: number.to_owned(),
-            track: track.to_owned(),
-        });
-    }
-}
-
-fn file_type(path: &str) -> Option<FileType> {
-    let ext = Path::new(&path).extension()?.to_str()?;
-
-    return match ext {
-        "flac" => Some(FileType::Flac),
-        "opus" => Some(FileType::Opus),
-        _ => None,
-    };
-}
+mod tag;
 
 fn tidy_string(string: &str) -> String {
-    return diacritics::remove_diacritics(string)
+    let tidied = diacritics::remove_diacritics(string)
         .nfc()
-        .to_string()
-        .replace(r#"""#, "")
-        .replace(":", "")
-        .replace("?", "")
-        .replace("'", "");
+        .collect::<String>();
+
+    let remove_regex = Regex::new(r#"[":?']"#).unwrap();
+    let removed = remove_regex.replace_all(&tidied, "").into_owned();
+
+    let replace_regex = Regex::new(r#"[><|]"#).unwrap();
+    return replace_regex.replace_all(&removed, "-").into_owned();
 }
 
-fn process_file(base: &str, path: &str) -> Result<()> {
-    let tag = match Tag::read(path) {
-        Ok(tag) => tag,
-        Err(_) => {
-            eprintln!("Error reading tag: {}", path);
-            return Err(TagError::ReadError);
-        }
-    };
+fn process_file(base: &str, path: &str) -> tag::Result<()> {
+    let tag = tag::Tag::read(path)?;
 
     let extension = Path::new(&path)
         .extension()
@@ -189,7 +63,10 @@ fn main() {
         .filter_map(|e| e.ok())
     {
         if let Some(path) = entry.path().to_str() {
-            process_file(&canonical_string, &path).ok();
+            match process_file(&canonical_string, &path) {
+                Ok(_) => (),
+                Err(_) => eprintln!("Error reading tag: {}", path),
+            }
         }
     }
 }
---
+++ src/tag.rs
@@ -0,0 +1,121 @@
+use regex::Regex;
+use std::collections::HashMap;
+use std::io;
+use std::num;
+use std::path::Path;
+
+pub enum TagError {
+    ReadError,
+}
+
+impl From<num::ParseIntError> for TagError {
+    fn from(_err: num::ParseIntError) -> TagError {
+        TagError::ReadError
+    }
+}
+
+impl From<io::Error> for TagError {
+    fn from(_err: io::Error) -> TagError {
+        TagError::ReadError
+    }
+}
+
+impl From<opus_headers::ParseError> for TagError {
+    fn from(_err: opus_headers::ParseError) -> TagError {
+        TagError::ReadError
+    }
+}
+
+impl From<metaflac::Error> for TagError {
+    fn from(_err: metaflac::Error) -> TagError {
+        TagError::ReadError
+    }
+}
+
+pub type Result<T> = std::result::Result<T, TagError>;
+
+enum FileType {
+    Opus,
+    Flac,
+}
+
+pub struct Tag {
+    pub album: String,
+    pub artist: String,
+    // disc: Option<i8>,
+    pub number: i32,
+    pub track: String,
+}
+
+fn extract_tag<'a>(tag: &'a HashMap<String, Vec<String>>, key: &str) -> Result<&'a String> {
+    return tag
+        .get(key)
+        .ok_or(TagError::ReadError)?
+        .get(0)
+        .ok_or(TagError::ReadError);
+}
+
+fn parse_number(tag: &str) -> Result<i32> {
+    let number_regex = Regex::new(r"[^0-9].*").unwrap();
+    return number_regex
+        .replace_all(tag, "")
+        .parse::<i32>()
+        .map_err(|_| TagError::ReadError);
+}
+
+fn file_type(path: &str) -> Option<FileType> {
+    let ext = Path::new(&path).extension()?.to_str()?;
+
+    return match ext {
+        "flac" => Some(FileType::Flac),
+        "opus" => Some(FileType::Opus),
+        _ => None,
+    };
+}
+
+impl Tag {
+    pub fn read(path: &str) -> Result<Tag> {
+        match file_type(path) {
+            Some(FileType::Flac) => Tag::read_flac(path),
+            Some(FileType::Opus) => Tag::read_opus(path),
+            _ => Err(TagError::ReadError),
+        }
+    }
+
+    fn read_flac(path: &str) -> Result<Tag> {
+        let mut tag = metaflac::Tag::read_from_path(&path)?;
+        let comments = &tag.vorbis_comments_mut().comments;
+        let artist = extract_tag(&comments, "ARTIST")?;
+        let album = extract_tag(&comments, "ALBUM")?;
+        let track = extract_tag(&comments, "TITLE")?;
+        let number = extract_tag(&comments, "TRACKNUMBER").and_then(|t| parse_number(t))?;
+
+        return Ok(Tag {
+            artist: artist.to_owned(),
+            album: album.to_owned(),
+            // disc: None,
+            number: number.to_owned(),
+            track: track.to_owned(),
+        });
+    }
+
+    fn read_opus(path: &str) -> Result<Tag> {
+        let headers = opus_headers::parse_from_path(path)?;
+        let comments = headers.comments.user_comments;
+        let artist = comments.get("ARTIST").ok_or(TagError::ReadError)?;
+        let album = comments.get("ALBUM").ok_or(TagError::ReadError)?;
+        let track = comments.get("TITLE").ok_or(TagError::ReadError)?;
+        let number = comments
+            .get("TRACKNUMBER")
+            .ok_or(TagError::ReadError)
+            .and_then(|t| parse_number(t))?;
+
+        return Ok(Tag {
+            artist: artist.to_owned(),
+            album: album.to_owned(),
+            // disc: None,
+            number: number.to_owned(),
+            track: track.to_owned(),
+        });
+    }
+}
