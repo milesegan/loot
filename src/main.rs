d5f83b69c5e74687b187f59986a27f626732aefe
Miles Egan <miles@plastaq.com>
2022-06-30 14:51:23 -0500

    Tag logic fix

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,9 @@ use std::path::Path;
 mod tag;
 
 fn tidy_string(string: &str) -> String {
-    let tidied = string.trim().to_lowercase();
-    let no_diacritics = deunicode::deunicode_with_tofu(&tidied, "_");
-    let remove_regex = Regex::new(r#"[":\?\*'!,&\-\(\)\.]"#).unwrap();
+    let tidied = string.trim();
+    let no_diacritics = deunicode::deunicode_with_tofu(&tidied, "_").to_lowercase();
+    let remove_regex = Regex::new(r#"[":;\?\*'!,&\-\(\)\.]"#).unwrap();
     let removed = remove_regex.replace_all(&no_diacritics, "").into_owned();
     let replace_regex = Regex::new(r#"[/><|\\ ]+"#).unwrap();
     return replace_regex.replace_all(&removed, "_").into_owned();
