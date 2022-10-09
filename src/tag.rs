96def52a64de7aa44cff0cd8f065d36acc20f10f
Miles Egan <miles@plastaq.com>
2022-10-09 12:56:34 -0500

    use primary tag

--- src/tag.rs
+++ src/tag.rs
@@ -6,6 +6,6 @@ use crate::error::{AppError, Result};
 
 pub fn read(path: &Path) -> Result<Tag> {
     let tagged_file = read_from_path(path, false)?;
-    let tag = tagged_file.first_tag().ok_or(AppError::ReadTagError)?;
+    let tag = tagged_file.primary_tag().ok_or(AppError::ReadTagError)?;
     return Ok(tag.to_owned());
 }
