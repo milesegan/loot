5bfabf10faf304ec9100be96e5bcfb0f49c67c93
Miles Egan <miles@plastaq.com>
2024-01-06 14:51:53 +0700

    Preserve mtime on transcode

--- src/transcode.rs
+++ src/transcode.rs
@@ -1,5 +1,7 @@
+use filetime::FileTime;
 use globwalk::DirEntry;
 use rayon::prelude::*;
+use std::fs;
 use std::path::Path;
 
 use crate::tag;
@@ -12,8 +14,9 @@ pub enum TranscodeFormat {
 }
 
 fn transcode_file(source: &Path, dest: &Path, format: TranscodeFormat) -> std::io::Result<()> {
-    std::fs::remove_file(dest).ok();
+    fs::remove_file(dest).ok();
     let tmp = tempfile::NamedTempFile::new()?;
+    let source_meta = fs::metadata(source)?;
 
     let child = match format {
         TranscodeFormat::Opus => std::process::Command::new("ffmpeg")
@@ -75,17 +78,21 @@ fn transcode_file(source: &Path, dest: &Path, format: TranscodeFormat) -> std::i
             .expect("failed to execute child"),
     };
     child.wait_with_output().expect("failed to wait on child");
-    std::fs::create_dir_all(dest.parent().unwrap()).expect("Error making dest dir");
-    std::fs::rename(tmp.path(), dest).expect("Error moving file");
+    fs::create_dir_all(dest.parent().unwrap()).expect("Error making dest dir");
+    fs::rename(tmp.path(), dest).expect("Error moving file");
     match format {
         TranscodeFormat::Aac => tag::copy(source, dest).expect("Error copying tag"),
         _ => (),
     }
+
+    let mtime = FileTime::from_last_modification_time(&source_meta);
+    filetime::set_file_mtime(dest, mtime)?;
+
     return Ok(());
 }
 
 fn extract_cover(source: &Path, dest: &Path) -> std::io::Result<()> {
-    std::fs::create_dir_all(dest.parent().unwrap()).expect("Error making dest dir");
+    fs::create_dir_all(dest.parent().unwrap()).expect("Error making dest dir");
     let child = std::process::Command::new("ffmpeg")
         .arg("-y")
         .arg("-loglevel")
