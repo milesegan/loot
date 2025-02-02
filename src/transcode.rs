3312b6ade98ad934cbb8ca2b653e5f08b066c838
Miles Egan <miles@plastaq.com>
2025-02-02 16:20:31 +0700

    encode opus at 128

--- src/transcode.rs
+++ src/transcode.rs
@@ -50,7 +50,7 @@ fn transcode_file(source: &Path, dest: &Path, format: TranscodeFormat) -> std::i
             .arg("-map")
             .arg("a:0")
             .arg("-b:a")
-            .arg("192k")
+            .arg("128k")
             .arg("-f")
             .arg("opus")
             .arg(tmp.as_path())
